9574669c0bbdec524476b5204e464715a956c799
logapradeep <logapradeep@gmail.com>
2026-03-20 01:29:49 +0530

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="menvayal-agent",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "paho-mqtt>=2.0.0",
